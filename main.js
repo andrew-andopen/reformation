c6f2a427bcd2373f25f4eddd5ac493b4aa395630
Andrew Shannon <andrew@andopen.co>
2020-08-21 15:01:47 +0100

    added social

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ const h1Tag = document.querySelector('h1')
 const h2Tag = document.querySelector('h2')
 const textAreaTag = document.querySelector('textarea')
 const submitTag = document.querySelector("a")
+const socialBlock = document.querySelector(".social")
+
 
 
 
@@ -22,7 +24,7 @@ if (xhr.status === 200) {
     textAreaTag.style.display = "none"
     submitTag.innerHTML = "Book class"
     submitTag.setAttribute("href", "https://clients.mindbodyonline.com/classic/mainclass?studioid=435666&tg=31&vt=&lvl=&stype=-7&view=day&trn=0&page=&catid=&prodid=&date=8%2f21%2f2020&classid=0&prodGroupId=&sSU=&optForwardingLink=&qParam=&justloggedin=&nLgIn=&pMode=0&loc=1");
-    
+    socialBlock.style.display = "block"
 } else {
   formH1Tag.innerHTML = `Sorry, please reload your page and try again`
 }
@@ -34,7 +36,6 @@ xhr.send(formData);
 //Submit negative form
 const formNeg = document.getElementById("form-neg")
 
-console.log(formNeg)
 
 formNeg.onsubmit = function(event) {
 event.preventDefault();
@@ -46,9 +47,7 @@ console.log(xhr);
 var response = JSON.parse(xhr.response);
 if (xhr.status === 200) {
     h1Tag.innerHTML = "Thanks for your feedback.";
-    h2Tag.innerHTML = "Lorem ipsum dolor sit amet, consectetur adipiscing elit";
-    textAreaTag.style.display = "none"
-    submitTag.style.display = "none"
+    h2Tag.innerHTML = "Lorem ipsum dolor sit amet, consectetur adipiscing elit"
     
     
 } else {
